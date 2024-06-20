a270493c18193acabc4caa670a2c39ff6ca0fc8e
bensteUEM <Benedict.Stein@gmail.com>
2024-06-20 21:02:35 +0200

    minor linting change

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = require('@companion-module/tools/eslint/index.cjs')({
-  // Enable support for jest globals in test files
-  enableJest: false,
-  // Enable support for typescript
-  enableTypescript: false,
+	// Enable support for jest globals in test files
+	enableJest: false,
+	// Enable support for typescript
+	enableTypescript: false,
 })
